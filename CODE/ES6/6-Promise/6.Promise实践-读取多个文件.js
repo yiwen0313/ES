f5ee124be84ab41311712f7b6120180f4b478437
yiwen0313 <2469119402@qq.com>
2021-09-25 15:12:17 +0800

    Promise实践练习-多个文件内容读取 学习完成

---
+++ CODE/ES6/6-Promise/6.Promise实践-读取多个文件.js
@@ -0,0 +1,38 @@
+// 引入 fs 模块
+const fs = require('fs');
+
+// 普通方法
+/*fs.readFile('./resources/为学.md', (err, data1) => {
+    fs.readFile('./resources/插秧诗.md', (err, data2) => {
+        fs.readFile('./resources/观书有感.md', (err, data3) => {
+            let result = data1 + '\r\n' + data2 + '\r\n' + data3;
+            console.log(result);
+        });
+    });
+});*/
+
+
+// 使用 Promise 实现
+const p = new Promise((resolve, reject) => {
+    fs.readFile('./resources/为学.md', (err, data) => {
+        resolve(data);
+    });
+});
+
+p.then(value => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./resources/插秧诗.md', (err, data) => {
+            resolve([value, data]);
+        });
+    });
+}).then(value => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./resources/观书有感.md', (err, data) => {
+            //压入
+            value.push(data);
+            resolve(value);
+        });
+    });
+}).then(value => {
+    console.log(value.join('\r\n'));
+});
