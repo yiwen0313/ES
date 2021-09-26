1162a689b7afa7fa741e5af1536479a9698ae617
yiwen0313 <2469119402@qq.com>
2021-09-26 17:01:34 +0800

    ES8-async和await结合读取文件 学习完成

---
+++ CODE/ES8/3.async和await结合读取文件.js
@@ -0,0 +1,52 @@
+// 引入 fs 模块
+const fs = require("fs");
+
+// 读取 ‘为学’
+function readWeiXue() {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./resources/为学.md", (err, data) => {
+            // 如果失败
+            if (err) reject(err);
+            // 如果成功
+            resolve(data);
+        });
+    });
+}
+
+// 读取 ‘插秧诗’
+function readChaYangShi() {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./resources/插秧诗.md", (err, data) => {
+            // 如果失败
+            if (err) reject(err);
+            // 如果成功
+            resolve(data);
+        });
+    });
+}
+
+// 读取 ‘观书有感’
+function readGuanShu() {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./resources/观书有感.md", (err, data) => {
+            // 如果失败
+            if (err) reject(err);
+            // 如果成功
+            resolve(data);
+        });
+    });
+}
+
+// 声明一个 async 函数
+async function main() {
+    // 获取为学内容
+    let weixue = await readWeiXue();
+    let chayang = await readChaYangShi();
+    let guanshu = await readGuanShu();
+
+    console.log(weixue.toString());
+    console.log(chayang.toString());
+    console.log(guanshu.toString());
+}
+
+main();
