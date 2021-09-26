b5dbfb91a5f00058c6bc8c8f80fd53238d862e6b
yiwen0313 <2469119402@qq.com>
2021-09-26 14:46:11 +0800

    ES6模块化引入npm包 学习完成

--- CODE/ES6/11-Module/src/js/app.js
+++ CODE/ES6/11-Module/src/js/app.js
@@ -9,6 +9,10 @@ import * as m3 from "./m3.js";
 // console.log(m2);
 // console.log(m3);
 
-m1.teach();
-m2.findJob();
-m3.default.change();
+// m1.teach();
+// m2.findJob();
+// m3.default.change();
+
+// 修改背景颜色为粉色
+import $ from 'jquery'; // const $ = require('jquery);
+$('body').css('background', 'pink');
