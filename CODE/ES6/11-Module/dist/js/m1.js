1caed19b20c730eb83f499f819fabd8abbae5ebd
yiwen0313 <2469119402@qq.com>
2021-09-26 12:13:00 +0800

    bable对ES6模块化代码转换 学习完成

---
+++ CODE/ES6/11-Module/dist/js/m1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.teach = teach;
+// 分别暴露 
+var school = exports.school = 'xpu';
+
+function teach() {
+    console.log("我们可以教给你知识");
+}
---
+++ CODE/ES6/11-Module/dist/js/m3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+// 默认暴露
+exports.default = {
+    school: 'XPU',
+    change: function change() {
+        console.log("大学生需要体侧");
+    }
+};
